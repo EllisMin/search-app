553e738073a30392cdbf33f93bfde3e941dbe35a
Ellis Min <dbmin@hotmail.com>
2019-10-31 23:35:03 -0500

    adding files for github page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* Component is react lib */
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      searchBox: ""
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users2 => this.setState({ users: users2 }));
+
+
+    // Fetch local json file
+    // fetch("http://localhost:3000/fakejsondata.json")
+    // .then(response => response.json())
+    // .then(users2 => this.setState({ users: users2 }));
+    
+  }
+
+  handleChange = e => {
+    this.setState({ searchBox: e.target.value });
+  };
+
+  render() {
+    // Pulling the property off of state
+    const { users, searchBox } = this.state;
+    const filteredUsers = users.filter(user =>
+      user.name.toLowerCase().includes(searchBox.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Searching user demo with React</h1>
+        <SearchBox
+          placeholder="search users"
+          handleChange={this.handleChange}
+        />
+        <CardList users={filteredUsers} />
+      </div>
+    );
+  }
 }
 
 export default App;

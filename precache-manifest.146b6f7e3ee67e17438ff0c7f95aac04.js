2421ff88aaf112338a1226ddfc7475048e6c31a2
Ellis Min <dbmin@hotmail.com>
2019-10-31 23:33:21 -0500

    Updates

---
+++ precache-manifest.146b6f7e3ee67e17438ff0c7f95aac04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7dab1134d17258b0f1406f7327fd6c7",
+    "url": "/search-app/index.html"
+  },
+  {
+    "revision": "1287a4d00b2afc14b45f",
+    "url": "/search-app/static/css/main.df043039.chunk.css"
+  },
+  {
+    "revision": "0cca02fe10f4d25a3546",
+    "url": "/search-app/static/js/2.e74b291d.chunk.js"
+  },
+  {
+    "revision": "1287a4d00b2afc14b45f",
+    "url": "/search-app/static/js/main.47fedeed.chunk.js"
+  },
+  {
+    "revision": "7f233e4bd61fc14e60b1",
+    "url": "/search-app/static/js/runtime-main.5d0f6160.js"
+  }
+]);
